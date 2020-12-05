89fe1bb7318f6871e38928645e111fe6525ac603
jad111 <jad111@duke.edu>
2020-12-05 14:00:19 -0500

    Rename endpoint

--- django-react-proj/goals/urls.py
+++ django-react-proj/goals/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('api/goal/lc/', views.GoalListCreate.as_view()),
-    path('api/goal/<int:pk>', views.GoalRetrieveUpdateDestroy.as_view()),
+    path('api/goals/', views.GoalListCreate.as_view()),
+    path('api/goals/<int:pk>', views.GoalRetrieveUpdateDestroy.as_view()),
 ]
