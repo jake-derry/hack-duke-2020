175fe7ffb7da738155e732e261c45e4b1bffc26c
Will Monahan <william.monahan@duke.edu>
2020-12-06 11:17:27 -0500

    Fixed URL error

--- django-react-proj/users/urls.py
+++ django-react-proj/users/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path('api/counselors/me/', views.CounselorView.as_view()),
     path('api/counselors/me/tracks/', views.CounselorTrackLC.as_view()),
     path('api/counselors/me/tracks/<int:pk>', views.CounselorTrackRUD.as_view()),
-    path('api/counselors/me/students/', views.CounselorStudentsListCreate.as_view()),
+    path('api/counselors/me/students/', views.CounselorStudentsList.as_view()),
     path('api/students/me/', views.StudentView.as_view()),
     path('api/goals/', views.GoalListCreate.as_view()),
     path('api/goals/<int:pk>', views.GoalRetrieveUpdateDestroy.as_view()),
