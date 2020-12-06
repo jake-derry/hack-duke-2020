0c3311fa0bf4441204e24c6d179888e077bbf206
han5010 <error404@live.unc.edu>
2020-12-06 10:50:59 -0500

    no change just test

--- frontend/fe/src/pages/cpage/cpagescreen.js
+++ frontend/fe/src/pages/cpage/cpagescreen.js
@@ -10,25 +10,28 @@ function CPageScreen() {
             <div class="row" >
                 <div class="col-lg-3 bg-light">
                     <div class="btn btn-danger">
-                        <p style={{fontWeight: "bold"}}>- Remove Group</p>
+                        <p style={{fontWeight: "bold"}}>- Rem Group</p>
                     </div>
-                    <p>Class of 2020</p>
-                    <p>Class of 2021</p>
-                    <p>Class of 2022</p>
-                    <p>Class of 2023</p>
+                    <p class="selectgroup">Class of 2020</p>
+                    <p class="selectgroup">Class of 2021</p>
+                    <p className="selectgroup">Class of 2022</p>
+                    <p className="selectgroup">Class of 2023</p>
                     <div class="btn btn-success">
                         <p style={{fontWeight: "bold"}}>+ New Group</p>
                     </div>
                 </div>
                 <div class="col-lg-9 bg-primary">
-                    <h1>Counselor Page</h1>
-                    <p>Table of students will be rendered here</p>
+                    <Link to='/clogin'>
+                        <li class="btn btn-danger" style={{float: "right", marginRight: "10px", marginTop: '10px'}}>Log Out</li>
+                    </Link>
+                    <div className="cpagecontent">
+                        <h1>Counselor Page</h1>
+                        <p>Table of students will be rendered here</p>
+                    </div>
                     <Link to='/students'>
                         <li>Students</li>
                     </Link>
-                    <Link to='/clogin'>
-                        <li class="btn btn-danger" style={{float: "right", marginRight: "10px"}}>Log Out</li>
-                    </Link>
+                  
                     
                 </div>
             </div>
